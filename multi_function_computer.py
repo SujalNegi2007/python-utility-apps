340a804b9a88425cd33d3e68edc280ee5b846670
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-05-25 13:41:50 +0530

    Update multi_function_computer.py
    Made all options in the menu avaliable.

--- multi_function_computer.py
+++ multi_function_computer.py
@@ -1,6 +1,6 @@
-import string
+import math
 def window():
-    print("+-------------------------------------------------+\n| To Add Numbers                      : Enter [1] |\n| To Subtract Numbers                 : Enter [2] |\n| To Multiply Numbers                 : Enter [3] |\n| To Divide Numbers                   : Enter [4] |\n| To Use Power in Number              : Enter [5] |\n| To Find Module of  Numbers          : Enter [6] |\n| To Find Average of  Numbers         : Enter [7] |\n| To Find Biggest Number b/w Numbers  : Enter [8] |\n| To Find Smallest Number b/w Numbers : Enter [9] |\n| To Find Square root of Number      : Enter [10] |\n| To View History                    : Enter [11] |\n| To Delete History                  : Enter [12] |\n| To Exit                            : Enter [13] |\n+-------------------------------------------------+\n")
+    print("+-------------------------------------------------+\n| To Add Numbers                      : Enter [1] |\n| To Subtract Numbers                 : Enter [2] |\n| To Multiply Numbers                 : Enter [3] |\n| To Divide Numbers                   : Enter [4] |\n| To Use Power in Number              : Enter [5] |\n| To Find Module of Numbers           : Enter [6] |\n| To Find Average of Numbers          : Enter [7] |\n| To Find Biggest Number b/w Numbers  : Enter [8] |\n| To Find Smallest Number b/w Numbers : Enter [9] |\n| To Find Square root of Number      : Enter [10] |\n| To View History                    : Enter [11] |\n| To Delete History                  : Enter [12] |\n| To Exit                            : Enter [13] |\n+-------------------------------------------------+\n")
     return
 #_______________________________________________________________________
 def user_input_main_menu():
@@ -19,16 +19,16 @@ def check_int(input_main_menu):
         print("Invalid Input! Error: Wrote something that is not a number!")
         return None , False
 #_______________________________________________________________________
-def main_menu():
+def main_menu(first_name):
     window()
     input_main_menu = user_input_main_menu()
     checking, is_right = check_int(input_main_menu)
     if is_right:
-        front_menu(checking)
+        front_menu(checking,first_name)
     else:
         print("Invalid Input!")
 #_______________________________________________________________________
-def front_menu(checking):
+def front_menu(checking,first_name):
     if checking == 1:
         output_front_menu = user_input_front_menu()
         sum_input(*output_front_menu)
@@ -43,31 +43,32 @@ def front_menu(checking):
         divide_input(*output_front_menu)
     elif checking == 5:
         output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        square_input(*output_front_menu)
     elif checking == 6:
         output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        module_input(*output_front_menu)
     elif checking == 7:
         output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        average_input(*output_front_menu)
     elif checking == 8:
         output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        max_input(*output_front_menu)
     elif checking == 9:
         output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        min_input(*output_front_menu)
     elif checking == 10:
         output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        sqrt_input(*output_front_menu)
     elif checking == 11:
-        output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        for key, value in History.items():
+            print(f"{key} => {value}")
     elif checking == 12:
-        output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        print("Clearing History...")
+        History.clear()
+        print("History Cleared!")
     elif checking == 13:
-        output_front_menu = user_input_front_menu()
-        #sum_input(*output_front_menu)
+        print("Exiting...")
+        return
 #_______________________________________________________________________
 def user_input_front_menu():
     input_front_menu = input("Enter the numbers seperated by [,]: ").split(",")
@@ -83,6 +84,7 @@ def user_input_main_menu():
 def sum_input(*output_front_menu):
     total_sum = sum(output_front_menu)
     print(f"Sum of Number is: {total_sum}")
+    History[first_name].append(f"Sum of Number is: {total_sum}")
     return
 #______________________________________________________________________
 def subtract_input(*output_front_menu):
@@ -90,13 +92,15 @@ def subtract_input(*output_front_menu):
     for i in range(1,len(output_front_menu)):
         total_subtraction -= output_front_menu[i]
     print(f"Subtract of Numbers are: {total_subtraction}")
+    History[first_name].append(f"Subtract of Numbers are: {total_subtraction}")
     return
 #______________________________________________________________________
 def multiply_input(*output_front_menu):
-    total_multiply = 1
-    for i in range(len(output_front_menu)):
-        total_subtraction *= output_front_menu[i]
-    print(f"Multiply of Numbers are: {total_subtraction}")
+    total_multiply = output_front_menu[0]
+    for i in range(1,len(output_front_menu)):
+        total_multiply *= output_front_menu[i]
+    print(f"Multiply of Numbers are: {total_multiply}")
+    History[first_name].append(f"Multiply of Numbers are: {total_multiply}")
     return
 #______________________________________________________________________
 def divide_input(*output_front_menu):
@@ -108,5 +112,64 @@ def divide_input(*output_front_menu):
             total_divide = "Not Defined"
             break
     print(f"Divide of Numbers are: {total_divide}")
+    History[first_name].append(f"Divide of Numbers are: {total_divide}")
     return
-main_menu()
+#______________________________________________________________________
+def square_input(*output_front_menu):
+    power = output_front_menu[0]
+    for i in range(1,len(output_front_menu)):
+        power **= output_front_menu[i]
+    print(f"Power of Numbers are: {power}")
+    History[first_name].append(f"Power of Numbers are: {power}")
+    return
+#______________________________________________________________________
+def module_input(*output_front_menu):
+    total_module = output_front_menu[0]
+    for i in range(1,len(output_front_menu)):
+        total_module %= output_front_menu[i]
+    print(f"Module of Numbers are: {total_module}")
+    History[first_name].append(f"Module of Numbers are: {total_module}")
+    return
+#______________________________________________________________________
+def average_input(*output_front_menu):
+    total_sum = sum(output_front_menu)
+    total_average = total_sum/len(output_front_menu)
+    print(f"Average of Numbers are: {total_average}")
+    History[first_name].append(f"Average of Numbers are: {total_average}")
+    return
+#______________________________________________________________________
+def max_input(*output_front_menu):
+    total_max = max(output_front_menu)
+    print(f"Biggest of Numbers are: {total_max}")
+    History[first_name].append(f"Biggest of Numbers are: {total_max}")
+    return
+#______________________________________________________________________
+def min_input(*output_front_menu):
+    total_min = min(output_front_menu)
+    print(f"Smallest of Numbers are: {total_min}")
+    History[first_name].append(f"Smallest of Numbers are: {total_min}")
+    return
+#______________________________________________________________________
+def sqrt_input(*output_front_menu):
+    box = []
+    for i in range(len(output_front_menu)):
+        box.append(math.sqrt(output_front_menu[i]))
+    print(f"Sqrt of following Numbers are: {box}")
+    History[first_name].append(f"Sqrt of following Numbers are: {box}")
+    return
+#______________________________________________________________________
+def name():
+    full_name = input("Enter your name: ").capitalize().strip()
+    name = full_name.split(" ")
+    first_name = name[0]
+    return first_name
+#----------------------------------------------------------------------
+History = {}
+while True:
+    first_name = name()
+    if first_name not in History:
+        History[first_name] = []
+        main_menu(first_name)
+    else:
+        print(f"Welcome back {first_name}!")
+        main_menu(first_name)
