6c4e0d4fb46d5c7593f447df29aab43ae7047c98
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-05-25 11:38:31 +0530

    Create multi_function_computer.py

---
+++ multi_function_computer.py
@@ -0,0 +1,48 @@
+import string
+def window():
+    print("+-------------------------------------------------+\n| To Add Numbers                      : Enter [1] |\n| To Subtract Numbers                 : Enter [2] |\n| To Multiply Numbers                 : Enter [3] |\n| To Divide Numbers                   : Enter [4] |\n| To Use Power in Number              : Enter [5] |\n| To Find Module of  Numbers          : Enter [6] |\n| To Find Average of  Numbers         : Enter [7] |\n| To Find Biggest Number b/w Numbers  : Enter [8] |\n| To Find Smallest Number b/w Numbers : Enter [9] |\n| To Find Square root of Number      : Enter [10] |\n| To View History                    : Enter [11] |\n| To Delete History                  : Enter [12] |\n| To Exit                            : Enter [13] |\n+-------------------------------------------------+\n")
+    return
+#_______________________________________________________________________
+def check_int(input_main_menu):
+    if input_main_menu.isdigit():
+        input_main_menu = int(input_main_menu)
+        if input_main_menu > 13 or input_main_menu < 1:
+            print("Invalid Input! Error: chose a number that is not b/w 1 to 13.")
+            return None , False
+        else:
+            return input_main_menu , True
+    else:
+        print("Invalid Input! Error: Wrote something that is not a number!")
+        return None , False
+#_______________________________________________________________________
+def main_menu():
+    window()
+    input_main_menu = user_input_main_menu()
+    checking, is_right = check_int(input_main_menu)
+    if is_right:
+        front_menu(checking)
+    else:
+        print("Invalid Input!")
+#_______________________________________________________________________
+def front_menu(checking):
+    if checking == 1:
+        output_front_menu = user_input_front_menu()
+        sum_input(*output_front_menu)
+#_______________________________________________________________________
+def user_input_front_menu():
+    input_front_menu = input("Enter the numbers seperated by [,]: ").split(",")
+    output_front_menu = []
+    for i in range(len(input_front_menu)):
+        output_front_menu.append(int(input_front_menu[i]))
+    return output_front_menu
+#_______________________________________________________________________
+def sum_input(*output_front_menu):
+    total_sum = sum(output_front_menu)
+    print(f"Sum of Number is: {total_sum}")
+    return
+#_______________________________________________________________________
+def user_input_main_menu():
+    input_main_menu = input("Enter the Number where you want to go: ")
+    return input_main_menu
+    
+main_menu()
